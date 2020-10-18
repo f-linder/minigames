ffb057533b09b5ac529322d1a36a391806081d0e
ge93wen <ge93wen@mytum.de>
2020-10-18 18:55:03 +0200

    snake

--- snake/snake.py
+++ snake/snake.py
@@ -1,8 +1,26 @@
 import pygame
+import os
 
+# initialize pygame
 pygame.init()
-
+# set dimensions
 window = pygame.display.set_mode((800, 800))
+# set caption and icon
 pygame.display.set_caption("Snake")
+icon = pygame.image.load(os.path.dirname(snake.py))
+pygame.display.set_icon(icon)
 
-pygame.quit()
+running = True
+
+# game loop
+while running:
+
+    # checking for input
+    for event in pygame.event().get():
+        # X pressed
+        if event == pygame.QUIT:
+            running = False
+
+
+    # update whats shown
+    pygame.display.update()
