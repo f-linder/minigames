3b3b063ceeb19a560db980c9af6878a8ff52582e
ge93wen <ge93wen@mytum.de>
2020-10-19 23:31:14 +0200

    added scoreboard and play again fucntionality

--- snake/snake.py
+++ snake/snake.py
@@ -23,6 +23,7 @@ color_food = (255, 112, 77)
 color_snake_body = (102, 255, 102)
 color_snake_head = (26, 255, 26)
 # boolean var for game loop
+play_again = True
 running = True
 # list for snake
 snake = []
@@ -30,7 +31,6 @@ snake = []
 snake.append((size_window - math.floor(columns / 2 - 2) * size_default, size_window - int(columns / 2) * size_default))
 # food + timer
 food = []
-missing_food = 0 
 ate = False
 # direction of motion
 motion = (1, 0)
@@ -38,22 +38,86 @@ motion = (1, 0)
 score = 0
 font = pygame.font.SysFont('Arial', 30, True, True)
 
-
 def game_loop():
-    while running:
-        pygame.time.delay(100)
-        check_events()
-        spawn_food()
-        move()
-        eating_food()
-        draw()
-    
+    global running, play_again
+    while play_again:
+        running = True
+        setup()
+        while running:
+            pygame.time.delay(100)
+            check_events()
+            spawn_food()
+            move()
+            eating_food()
+            draw()
+        play_again = end_screen()
+
+def setup():
+    global snake, food, score, ate, motion
+    score = 0
+    snake = []
+    snake.append((size_window - math.floor(columns / 2 - 2) * size_default, size_window - int(columns / 2) * size_default))
+    food = []
+    ate = False
+    motion = (1, 0)
+
+def end_screen():
+    global running, play_again
+    # draw end screen
+    window.fill(color_background)
+    # read scores from .csv file and update
+    highscore = False
+    scores = []
+    # read scores
+    with open('scores.csv', 'r') as file:
+        lines = file.readlines()
+        scores = [int(line.strip()) for line in lines]
+    # set new highscore
+    for i, num in enumerate(scores):
+        if num < score:
+            scores[i] = score
+            highscore = True
+            break 
+    # write new highscore to file
+    if highscore:
+        with open('scores.csv', 'w') as file:
+            for num in scores:
+                file.write(f'{str(num)}\n')
+    # display scores
+    score_font = pygame.font.SysFont('Arial', 50, True, False)
+    score_surface = score_font.render(f'Your Score is: {score}', False, color_snake_body)
+    window.blit(score_surface, (int(size_window * 0.15), int(size_window * 0.1)))
+    score_surface = score_font.render('Highscores:', False, color_food)
+    window.blit(score_surface, (int(size_window * 0.15), int(size_window * 0.25)))
+    once = True
+    for i, num in enumerate(scores):
+        if once and highscore and num == score:
+            score_surface = font.render(f'{i+1}.: {num}', False, color_snake_head)
+            once = False
+        else:
+            score_surface = font.render(f'{i+1}.: {num}', False, color_food)
+        window.blit(score_surface, (int(size_window * 0.6), int(size_window * 0.27) + (i * size_default * 3)))
+    score_surface = score_font.render('Press spacebar to', False, color_snake_head)
+    window.blit(score_surface, (int(size_window * 0.15), int(size_window * 0.8)))
+    score_surface = score_font.render('start a new round!', False, color_snake_head)
+    window.blit(score_surface, (int(size_window * 0.15), int(size_window * 0.88)))
+    pygame.display.update()
+    # check if user want to play again
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    return True
+
 def check_events():
-    global running, motion
+    global running, motion, play_again
     for event in pygame.event.get():
         # checking for quit
         if event.type == pygame.QUIT:
-            running == False  
+            running = False  
+            play_again = False
         # checking for movement
         elif event.type == pygame.KEYDOWN:
             # left
@@ -97,7 +161,6 @@ def move():
         snake.append(pos_last)
         ate = False
         
-
 def move_snake():
     global snake, running
     pos = 0
@@ -147,4 +210,4 @@ def draw():
 
 game_loop()
 pygame.quit()
-
+exit()
