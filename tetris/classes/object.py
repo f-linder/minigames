07548d954d6397114f2eb79452f04f975eddae79
Felix Linder <felix@pop-os.localdomain>
2020-11-14 23:08:50 +0100

    update

--- tetris/classes/object.py
+++ tetris/classes/object.py
@@ -1,9 +1,12 @@
 class Object:
-    def __init__(self):
+    def __init__(self, typ):
         self.pos = ()
         self.status = 0
-        self.width = None
-        self.height = None
+        self.num_blocks = 4
+        self.typ = typ
+        self.blocks = []
+    
+
     
     def move_down(array):
         return True
--- tetris/classes/tetris.py
+++ tetris/classes/tetris.py
@@ -11,7 +11,9 @@ class Tetris:
         image = pygame.image.load("tetris.png")
         pygame.display.set_icon(image)
         # colors
+        self.black = (0, 0, 0)
         self.color_background = (244, 228, 215)
+        self.color_border = (169, 169, 169)
         self.color_side = (105, 105, 105)
         self.color_i = (0, 191, 255)
         self.color_o = (255, 255, 0)
@@ -21,7 +23,6 @@ class Tetris:
         self.color_j = (65, 105, 225)
         self.color_l = (255, 165, 0)
         self.type_to_color = {
-            -1: self.color_background,
             0: self.color_i,
             1: self.color_o,
             2: self.color_t,
@@ -34,16 +35,18 @@ class Tetris:
         # sizes
         self.rows = 20
         self.columns = 10
+        self.bar_columns = 6
         self.pixel_per_casket = 30
         self.game_height = (self.rows + 2) * self.pixel_per_casket
         self.game_width = (self.columns + 2) * self.pixel_per_casket
-        self.bar_width = self.pixel_per_casket * 6
+        self.bar_width = self.pixel_per_casket * self.bar_columns
         # game variabels
         self.window = pygame.display.set_mode((self.game_width + self.bar_width, self.game_height))
         self.play_again = True
         self.running = True
         self.font = pygame.font.SysFont("Arial", 30, True, False)
         self.next = None
+        self.current = None
         self.array = []
         self.score = None
         self.score_lines = None
@@ -54,6 +57,7 @@ class Tetris:
         self.setup()
         while self.running:
             self.draw()
+            self.check_events()
         self.close()
 
     def check_events(self):
@@ -66,7 +70,8 @@ class Tetris:
         self.running = True
         self.score = 0
         self.score_lines = 0
-        self.next = Object()
+        self.next = Object(random.randint(0, 7))
+        self.current = Object(random.randint(0, 7))
         self.array = [[-1 for i in range(self.rows)] for j in range(self.columns)]
     
     def close(self):
@@ -78,7 +83,7 @@ class Tetris:
         self.window.fill(self.color_background)
 
         # drawing border
-        for x in range(self.columns + 2):
+        for x in range(self.columns + self.bar_columns + 2):
             # top
             self.draw_block(x - 1, -1, self.color_side)
             # bottom
@@ -86,15 +91,31 @@ class Tetris:
         for y in range(1, self.rows + 1):
             # left
             self.draw_block(-1, y - 1, self.color_side)
-            # right
+            # right middle
             self.draw_block(self.columns, y - 1, self.color_side)
+            # right
+            self.draw_block(self.columns + self.bar_columns, y - 1, self.color_side)
+
+        # horizontal lines at right
+        for x in range(self.columns + 2, self.bar_width + self.columns + 1):
+            self.draw_block(x - 1, 6, self.color_side)
         
         # drawing tetrominos
         for x, row in enumerate(self.array):
             for y, typ in enumerate(row):
-                self.draw_block(x, y, self.type_to_color[typ])
+                if typ != -1:
+                    self.draw_block(x, y, self.type_to_color[typ])
 
         pygame.display.update()
 
     def draw_block(self, x, y, color):
-        pygame.draw.rect(self.window, color, ((x + 1) * self.pixel_per_casket, (y + 1) * self.pixel_per_casket, self.pixel_per_casket, self.pixel_per_casket), 3)
+        if color == self.color_side:
+            # side inside
+            pygame.draw.rect(self.window, self.color_border, ((x + 1) * self.pixel_per_casket, (y + 1) * self.pixel_per_casket, self.pixel_per_casket, self.pixel_per_casket))
+            # side border
+            pygame.draw.rect(self.window, color, ((x + 1) * self.pixel_per_casket, (y + 1) * self.pixel_per_casket, self.pixel_per_casket, self.pixel_per_casket), 8)
+        else:
+            # normal inside
+            pygame.draw.rect(self.window, color, ((x + 1) * self.pixel_per_casket, (y + 1) * self.pixel_per_casket, self.pixel_per_casket, self.pixel_per_casket))
+            # normal border
+            pygame.draw.rect(self.window, self.black, ((x + 1) * self.pixel_per_casket, (y + 1) * self.pixel_per_casket, self.pixel_per_casket, self.pixel_per_casket), 3)
